a8f493a0edc0b4590ed8b91330efce02c1303527
Cai heng <ianyaheng822@gmail.com>
2020-06-09 17:06:27 +1000

    add the header

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,43 @@
 <template>
   <div id="app">
- 
-    <Todos v-bind:todos="todos"/>
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
-
+import Header from './components/layout/Header';
 export default {
-  name: 'App',
-  components:{
-    Todos
-  },
-  data(){
-    return {
-      todos:[
-        {
-          id:1,
-          title:'list1',
-          completed: false
-        },
-        {
-          id:2,
-          title:'list2',
-          completed: true
-        },
-        {
-          id:3,
-          title:'list3',
-          completed: false
-        },
-      ]
-    }
+  name:"app",
+  components: {
+    Header
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
